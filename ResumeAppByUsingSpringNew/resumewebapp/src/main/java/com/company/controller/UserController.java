9d2318a6646568ec25fadeff191b33f707e2328f
Sarkhan Rasullu <sarkhanrasullu@gmail.com>
2019-03-29 01:05:43 +0400

    changes

--- ResumeAppByUsingSpringNew/resumewebapp/src/main/java/com/company/controller/UserController.java
+++ ResumeAppByUsingSpringNew/resumewebapp/src/main/java/com/company/controller/UserController.java
@@ -1,15 +1,14 @@
 package com.company.controller;
 
 import com.company.entity.User;
+import com.company.form.UserForm;
 import com.company.service.inter.UserServiceInter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -43,9 +42,28 @@ public class UserController {
         List<User> list = userService.getAll(name, surname, nationalityId);
         ModelAndView mv = new ModelAndView("users");
 
-        
-        mv.addObject("list",list);
+        mv.addObject("users",list);
         return mv;
     }
 
+    @RequestMapping(method = RequestMethod.GET, value="/usersm")//users?name=Sarkhan
+        public ModelAndView indexM(
+                @Valid
+                @ModelAttribute("user") UserForm u,
+                BindingResult bindingResult){
+        ModelAndView mv = new ModelAndView("users");
+        if (bindingResult.hasErrors()) {
+            return mv;
+        }
+
+        List<User> list = userService.getAll(u.getName(), u.getSurname(), u.getNationalityId());
+        mv.addObject("users", list);
+        return mv;
+    }
+
+
+    @ModelAttribute("user")
+    public UserForm getEmptyUserForm(){
+       return new UserForm(null, null,null);
+    }
 }
